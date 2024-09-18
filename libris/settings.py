aebffea056f45e82463616a353491fcc5394bf01
abusaidshabib <abusaidshabib712@gmail.com>
2024-09-18 11:27:05 +0600

    configure done

--- libris/settings.py
+++ libris/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATE_DIR = BASE_DIR / "templates"
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    "django_browser_reload",
+
+    'authentication'
 ]
 
 MIDDLEWARE = [
@@ -47,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = 'libris.urls'
---
+++ readne.md
@@ -0,0 +1,3 @@
+npm i (if node module folder doesn't exist)
+npx tailwindcss -i ./static/cs/style.css -o ./static/cs/output.css --watch
+python manage.py runserver
